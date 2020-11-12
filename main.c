5f62f5e42efd96375ffd459a4f6581be19243b7f
Henk van de Berg <11218586+holkerveen@users.noreply.github.com>
2020-11-12 11:45:37 +0100

    Implemented simple prompt Included command for switching light on pin RB3 (on/off/toggle)

--- main.c
+++ main.c
@@ -15,6 +15,8 @@
 #define _XTAL_FREQ 10000000 // 1 instruction cycle = 1us
 
 #include <xc.h>
+#include <string.h>
+#include <stdlib.h>
 
 void serialChr(char chr) {
     while(!TXIF);
@@ -24,21 +26,19 @@ void serialStr(char str[]) {
     for(int i=0; str[i]!=0; i++)
         serialChr(str[i]);
 }
-void main(void) {
-    // Set led pin as output
-    TRISB0 = 0;
-    
-    // Enable USART
-    TRISB1 = 1;
-    TRISB2 = 1;
-    SPBRG = 10;
-    TXSTA = 0b00100100;
-    RCSTA = 0b10010000;
-     
+void writeLine(char str[]) {
+    serialStr(str);
     serialStr("\r\n");
-    serialStr("PicShell> ");
-    while (1) {
+}
 
+
+char *prompt(char msg[]) {
+    serialStr(msg);
+    static char buf[41];
+    memset(buf,0,sizeof buf);
+    char len = 0;
+    
+    while(1) {       
         if(RCIF) {
             if(OERR) {
                 __delay_ms(100);
@@ -50,9 +50,63 @@ void main(void) {
                 RCREG;
                 serialStr("FERR");
             }
-            serialChr(RCREG);
+            char chr = RCREG;
+            
+            if(chr == '\r') {
+                serialStr("\r\n");
+                return buf;
+            }
+            if(chr >= 0x20 || chr < 0x7f) {
+                if(strlen(buf) >= 40) {
+                    continue;
+                }
+                buf[len++] = chr;
+                serialChr(chr);
+            }
+            if(chr == 0x7) {
+                buf[--len] = 0;
+            }            
+        }
+    }
+}
+
+_Bool streq(char *a, char *b) {
+    return stricmp(a,b)== 0;
+}
+
+void main(void) {
+    // Set led pin as output
+    TRISB3 = 0;
+    
+    // Enable USART
+    TRISB1 = 1;
+    TRISB2 = 1;
+    SPBRG = 10;
+    TXSTA = 0b00100100;
+    RCSTA = 0b10010000;
+    
+    serialStr("\r\n");
+    while (1) {
+        char *line = prompt("PicShell> ");
+
+//        serialStr("Rx: ");
+//        writeLine(line);
+        
+        char *cmd = strtok(line," ");
+        
+        // Light control
+        if(streq("light", cmd)) {
+            char *mode = strtok(NULL," ");
+            if(streq("on",mode)) RB3 = 1;
+            else if(streq("off",mode)) RB3 = 0;
+            else if(streq("toggle",mode)) RB3 = !RB3;
+            else writeLine("Unknown parameter");
         }
         
+        // Command unknown
+        else {
+            writeLine("Unknown command");
+        }
 
     }
 }
